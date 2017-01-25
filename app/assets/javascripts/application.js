5d774ebdcf81487f274c178aac0957451159b2db
Julia Dickey <Julia@Julias-MacBook-Pro.local>
2017-01-25 12:19:33 -0800

    Add bootstrap and play with styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require turbolinks
+//= require jquery_ujs
+//= require jquery
+//= require bootstrap-sprockets
 //= require moment
 //= require fullcalendar
 //= require_tree .
