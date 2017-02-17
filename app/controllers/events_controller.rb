8950f84577171c05d1afbc2db01dc236fe4618eb
Julia Dickey <Julia@Julias-MacBook-Pro.local>
2017-02-16 17:58:21 -0800

    Update fonts on calendar to match styles set in member views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,33 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
-  # respond_to do |format|
-  #   format.html
-  #   format.json { render json:@events.to_json }
-  # end
-  #
-  # def self.between(start_time, end_time)
-  #   where('start_at > :lo and start_at < :up',
-  #     :lo => Event.format_date(start_time),
-  #     :up => Event.format_date(end_time)
-  #   )
-  # end
-  #
-  # def self.format_date(date_time)
-  #  Time.at(date_time.to_i).to_formatted_s(:db)
-  # end
-  #
-  # def as_json(options = {})
-  #   {
-  #     :id => self.id,
-  #     :title => self.title,
-  #     :start => start_at.rfc822,
-  #     :end => end_at.rfc822,
-  #     :allDay => allDay,
-  #     :url => Rails.application.routes.url_helpers.events_path(id),
-  #     :color => "green"
-  #   }
-  # end
 
   # GET /events
   # GET /events.json
@@ -61,6 +34,7 @@ class EventsController < ApplicationController
     @event = Event.new(event_params)
     @event.save
   end
+
 
   # PATCH/PUT /events/1
   # PATCH/PUT /events/1.json
